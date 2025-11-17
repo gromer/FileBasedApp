6c51ae5bfa78c35295c49f3fbc8460613ba2e586
Mike Gromer <385630+gromer@users.noreply.github.com>
2025-11-17 01:28:55 -0700

    Add initial project structure and core application classes
    - Introduced .gitignore for ignoring macOS specific files
    - Created solution file for the project
    - Added AsyncFileBasedApplication and FileBasedApplication classes with Build and ConfigureServices methods
    - Implemented FileBasedApplicationBase class for configuration and service setup
    - Updated project file with necessary package references

---
+++ FileBasedApp/FileBasedApplicationBase.cs
@@ -0,0 +1,35 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace FileBasedApp;
+
+public abstract class FileBasedApplicationBase
+{
+    protected IConfiguration? _configuration;
+
+    protected readonly IConfigurationBuilder _configurationBuilder;
+
+    protected readonly IServiceCollection _serviceCollection;
+
+    protected IServiceProvider? _serviceProvider;
+
+    public FileBasedApplicationBase()
+    {
+        this._configurationBuilder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile($"appsettings.{this.GetType().Name}.json",
+                optional: true,
+                reloadOnChange: true);
+
+        this._serviceCollection = new ServiceCollection();
+        this._serviceCollection.AddLogging(builder =>
+        {
+            builder.AddSimpleConsole();
+        });
+    }
+
+    public virtual void SetupBuild() { }
+
+    public virtual void SetupServices() { }
+}
